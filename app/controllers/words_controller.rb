a7d846b5ff2461259e56ace962d8b86ed27cd8ce
fred <fredy.rodriguez@pucp.pe>
2024-05-08 16:05:52 -0500

    fix: random_word method to return a word with a specific length

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,18 +1,32 @@
+require 'thread'
+require 'random_word'
 class WordsController < ApplicationController
+  RANDOM_WORDS_QUEUE = Queue.new
   def random_word
     length = params[:length].to_i
     word = nil
 
-    # Dequeue a random word from the thread-safe queue
-    if !RANDOM_WORDS_QUEUE.empty?
-      word = RANDOM_WORDS_QUEUE.pop
-    else
-      # Queue is empty, handle this case (e.g., refill the queue or return an error)
-      # For simplicity, you can refill the queue with all words again
-      RandomWord.nouns.each { |w| RANDOM_WORDS_QUEUE << w }
-      word = RANDOM_WORDS_QUEUE.pop unless RANDOM_WORDS_QUEUE.empty?
+    # Populate the queue with random words of the specified length during application startup
+    RandomWord.nouns(not_shorter_than: length, not_longer_than: length).each do |word|
+      RANDOM_WORDS_QUEUE << word
+    end
+    
+
+    # Dequeue a random word from the thread-safe queue matching the specified length
+    loop do
+      break if RANDOM_WORDS_QUEUE.empty?
+
+      candidate_word = RANDOM_WORDS_QUEUE.pop
+      if candidate_word.length == length
+        word = candidate_word
+        break
+      end
     end
 
-    render json: { word: word }
+    if word.nil?
+      render json: { error: "No words of length #{length} available in the queue" }, status: :not_found
+    else
+      render json: { word: word }
+    end
   end
 end
